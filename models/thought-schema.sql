cac2e742b2c4c582dc6fdf7d5437f1162d1b4844
Edwing123 <egarciavalle2014@gmail.com>
2021-09-02 01:18:53 -0600

    Create model/schema for thought table

---
+++ models/thought-schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE thought
+(
+    thought_id SMALLINT UNSIGNED AUTO_INCREMENT,
+    title VARCHAR(150),
+    message TEXT,
+    last_updated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    author VARCHAR(80),
+    CONSTRAINT pk_note PRIMARY KEY (thought_id)
+)
